34c033d9d9d5090bed2d4d405b4f3b6f0ebb8c47
cherian.nigel <cherian.nigel@outlook.com>
2018-09-19 01:03:21 -0600

    update on lesson 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -18,5 +18,32 @@ namespace Vidly.Controllers
             };
             return View(movie);
         }
+
+        //GET: Movies/Edit/id
+        //displays id = "id" on the new screen
+        //can also query string this now via edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //in this call, we can request all movies in the database
+        //defaults to page 1 if no index is passed)
+        //if no sortBy is passed through, then we sort by the movies by 
+        //their name automatically
+        //to make a parameter optional, we must first make it nullable
+        //this is implied via a "?" beside the datatype
+        //string types are a reference type in C# and are therefore nullable automatically
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            //for the purpose of this demo
+            return Content(String.Format("pageIndex={0} sortby={1}", pageIndex, sortBy));
+        }
     }
 }
